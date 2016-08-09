21bb742813e60ea14b7e5e61a820049b9eb72f64
sammyclark <iamsam10@hotmail.co.uk>
2016-08-09 16:32:22 +0100

    for loop working

--- 5. Mini Project Arrays/Random Winner Generator/rw.js
+++ 5. Mini Project Arrays/Random Winner Generator/rw.js
@@ -1,17 +1,18 @@
 var names = [];
-var textstring = " ";
+var textstring = "";
 
 names[0] = "Max";
 names[1] = "James";
 names[2] = "Zach";
 names[3] = "Sarah";
 //set global variables
+// array is working
 
-  //for (var n = 0; i < names.length; n++)
-    //{
-       // textstring += names[n] + " ";
-        document.getElementById("openspace").innerHTML = names;
-        //test for loop
+  for (var n = 0; n < names.length; n++)
+    {
+      textstring += names[n] + "";
+        //document.getElementById("openspace").innerHTML = names; - TEST FOR LOOP - for loop working
+        
     }
 //run array in a for loop
 
